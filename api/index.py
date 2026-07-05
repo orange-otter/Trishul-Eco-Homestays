76a351b1b08f3464a2ed3412a7bb00490ad6a340
orange-otter <patient.lama.parrot@gmail.com>
2026-07-05 12:58:58 +0530

    code ready for vercel deployment

--- api/index.py
+++ api/index.py
@@ -113,4 +113,4 @@ def delete_room(room_id: int):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=5000, reload=True)
+    uvicorn.run("index:app", host="0.0.0.0", port=5000, reload=True)
